06efd346afb149349bd961082c98151ef4747701
mdavies <mdavies@ebi.ac.uk>
2014-05-09 16:45:18 +0100

    Update KNIME package and a couple changes to more page

--- launchpad/more.php
+++ launchpad/more.php
@@ -39,12 +39,13 @@
           <h4><i class="fa fa-rocket"></i>&nbsp;Can you provide some more details on the ChEMBL database?</h4>
           <p>Have a look at the <a href="/assets/img/chembl_18_schema.png">ChEMBL schema diagram</a> to get an idea of the data captured in the database and how the tables join together. You can then start using the <a href="phppgadmin">phpPgAdmin Interface</a> to query the database directly with SQL. (Note the schema diagram does not include the RDKit tables - see next question)</p>
           
-          <h4><i class="fa fa-rocket"></i>&nbsp;What tables does the RDKit chemical cartridge add to the ChEMBL schema?</h4>
-          <p>Two additional tables are added to the ChEMBL schema after the RDKit cartridge is installed. These tables are:
+          <h4><i class="fa fa-rocket"></i>&nbsp;Are there any differences between the 'core' ChEMBL release database schema and the myChEMBL database schema?</h4>
+          <p>Yes, after we have installed the RDKit cartridge we create 2 additional tables, which are:
             <ul>
               <li><strong>mols_rdkit</strong> - contains the ChEMBL molecules RDKit representation in the column named 'm'.</li>
               <li><strong>fps_rdkit</strong> - contains six fingerprint representations of the ChEMBL molecules created using RDKit. For more details on the RDKit fingerprints please visit <a href="http://code.google.com/p/rdkit/wiki/FingerprintsInTheRDKit">this page</a>.</li>
             </ul>
+            Also, the <a href="/mychembl/app/home.php">myChEMBL Web Interface</a> creates a number of <strong>octmp_*</strong> temporary tables, which are used to cache search results. These tables can be ignored. 
           </p>
           
 

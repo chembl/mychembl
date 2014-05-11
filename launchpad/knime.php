7d3eef2f7b086c82baf3cc59f3fdb1f8b0bfca34
George Papadatos <gpapadatos@gmail.com>
2014-05-11 23:32:14 +0100

    more content

--- launchpad/knime.php
+++ launchpad/knime.php
@@ -17,11 +17,24 @@
           
           <h4><i class="fa fa-rocket"></i>&nbsp;How do I get KNIME?</h4>
           <p>Installing KNIME is really easy across all popular platforms (Windows, Mac OS, Linux). 
-          Just follow <a href="http://www.knime.org/node/81">this</a> link.</p>
+          Just follow <a href="http://www.knime.org/node/81" target="_blank">this</a> link.</p>
           
           <h4><i class="fa fa-rocket"></i>&nbsp;How do I find out more about KNIME?</h4>
-          <p>Here’s a useful <a href="http://www.knime.org/learning-hub">page</a>. 
-          For a basic introduction to KNIME followed by a few chemoinformatics applciations, see <a href="http://www.slideshare.net/gpapadatos/knime-tutorial">here</a>.</p>
+          <p>Here’s a useful <a href="http://www.knime.org/learning-hub" target="_blank">page</a>. 
+          For a basic introduction to KNIME followed by a few chemoinformatics applciations, see <a href="http://www.slideshare.net/gpapadatos/knime-tutorial" target="_blank">this</a> slideshow.</p>
+          
+          <h4><i class="fa fa-rocket"></i>&nbsp;How to I use KNIME with the myChEMBL VM?</h4>
+          <p>You can access the data in myChEMBL directly from a KNIME desktop running on your host machine. 
+          This can be achieved in two ways:
+          <ul>
+          <li>Direct database connection with the Database Reader node. 
+          The node can execute arbitrary SQL SELECT commands and retrieve the results in a KNIME table.</li>
+          <li>Calls to the RESTful <a href="/chemblws/docs" target="_blank">web services</a> that are hosted in myChEMBL. 
+          This is achieved with the KNIME REST (<a href="http://tech.knime.org/book/krest-rest-nodes-for-knime-trusted-extension" target="_blank">KREST</a>) nodes, which are available as community node contribution. 
+          The KREST nodes send the GET or POST requests to the server and format the response back to a KNIME table.</li>
+          For an example of the KNIME/myChEMBL connectivity, download this workflow to your host machine. 
+          Then, open your KNIME desktop, and click File --> Import KNIME workflow... --> Select archive file --> Browse.. and then navigate to the zip file you’ve just downloaded and click finish. If everything worked you will be able to see something that look like this on your KNIME desktop:
+          </p>
 
     
           <h4><i class="fa fa-rocket"></i>&nbsp;Downloads</h4>

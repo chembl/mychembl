71861a99bf90fb388e977c7daf30ab9dcf924570
George Papadatos <gpapadatos@gmail.com>
2014-05-11 23:20:21 +0100

    added KNIME content

--- launchpad/knime.php
+++ launchpad/knime.php
@@ -10,9 +10,20 @@
 
       <div class="row mychembl-faq">
         <div class="col-lg-12">
-          <h4><i class="fa fa-rocket"></i>&nbsp;Getting Started</h4>
-          <p>A KNIME quick start coming soon...</p>
+          <h4><i class="fa fa-rocket"></i>&nbsp;What is KNIME?</h4>
+          <p>KNIME is an open source modular graphical workbench for data processing and analysis. 
+          KNIME users can build workflows by executing predefined components, called <em>nodes</em>, in a process known as <em>visual programming</em>. 
+          Among other fields, KNIME has several extensions for life sciences, including bio- and chemoinformatics.</p>
+          
+          <h4><i class="fa fa-rocket"></i>&nbsp;How do I get KNIME?</h4>
+          <p>Installing KNIME is really easy across all popular platforms (Windows, Mac OS, Linux). 
+          Just follow <a href="http://www.knime.org/node/81">this</a> link.</p>
+          
+          <h4><i class="fa fa-rocket"></i>&nbsp;How do I find out more about KNIME?</h4>
+          <p>Here’s a useful <a href="http://www.knime.org/learning-hub">page</a>. 
+          For a basic introduction to KNIME followed by a few chemoinformatics applciations, see <a href="http://www.slideshare.net/gpapadatos/knime-tutorial">here</a>.</p>
 
+    
           <h4><i class="fa fa-rocket"></i>&nbsp;Downloads</h4>
           <p><a href="/assets/knime/myChEMBL18_KNIME.zip">myChEMBL18_KNIME.zip</a></p>
           

d09ce1236ac50707b17696465de4d2cd9becba17
Michal Nowotka <mnowotka@ebi.ac.uk>
2014-05-12 12:25:26 +0100

    some bilerplate vagrant conf added

---
+++ vagrant/cookbooks/fetch_cookbooks.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git clone git://github.com/opscode-cookbooks/build-essential.git
+git clone git://github.com/opscode-cookbooks/php.git
+git clone git://github.com/opscode-cookbooks/vim.git
+git clone git://github.com/opscode-cookbooks/git.git
+git clone git://github.com/hw-cookbooks/postgresql.git
+git clone git://github.com/koodough/chef-phppgadmin.git
+git clone git://github.com/onehealth-cookbooks/apache2.git
+git clone git://github.com/poise/python.git
+git clone git://github.com/rgbkrk/ipython-notebook-cookbook.git ipynb
+git clone git://github.com/poise/supervisor.git

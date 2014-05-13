c520e02bc590aab34dae80dfe6f2457060cdf3d8
Michał Nowotka <mnowotka@michas-mbp.windows.ebi.ac.uk>
2014-05-13 14:39:52 +0100

    removing phppgadmin recipe because is't poorely written

--- vagrant/cookbooks/fetch_cookbooks.sh
+++ vagrant/cookbooks/fetch_cookbooks.sh
@@ -24,7 +24,6 @@ git clone git://github.com/opscode-cookbooks/php.git
 git clone git://github.com/opscode-cookbooks/vim.git
 git clone git://github.com/opscode-cookbooks/git.git
 git clone git://github.com/hw-cookbooks/postgresql.git
-git clone git://github.com/koodough/chef-phppgadmin.git
 git clone git://github.com/onehealth-cookbooks/apache2.git
 git clone git://github.com/poise/python.git
 git clone git://github.com/rgbkrk/ipython-notebook-cookbook.git ipynb

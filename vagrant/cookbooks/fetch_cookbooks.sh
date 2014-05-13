7ff8e8c47fd4113169cebdbdd60372e7c6d83c9d
Michał Nowotka <mnowotka@michas-mbp.windows.ebi.ac.uk>
2014-05-13 10:16:02 +0100

    solving bugs, and missing dependencies

--- vagrant/cookbooks/fetch_cookbooks.sh
+++ vagrant/cookbooks/fetch_cookbooks.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+git clone git://github.com/opscode-cookbooks/ohai.git
+git clone git://github.com/opscode-cookbooks/rsyslog.git
+git clone git://github.com/opscode-cookbooks/bluepill.git
+git clone git://github.com/opscode-cookbooks/nginx.git
+git clone git://github.com/jesseadams/pacman.git
+git clone git://github.com/stevendanna/logrotate.git
+git clone git://github.com/opscode-cookbooks/openssl.git
+git clone git://github.com/opscode-cookbooks/xml.git
+git clone git://github.com/opscode-cookbooks/mysql.git
+git clone git://github.com/opscode-cookbooks/iis.git
 git clone git://github.com/opscode-cookbooks/chef_handler.git
 git clone git://github.com/hw-cookbooks/runit.git
 git clone git://github.com/opscode-cookbooks/iptables.git
 git clone git://github.com/opscode-cookbooks/windows.git
 git clone git://github.com/opscode-cookbooks/dmg.git
 git clone git://github.com/opscode-cookbooks/apt.git
-git clone https://github.com/opscode-cookbooks/yum-epel.git
+git clone git://github.com/opscode-cookbooks/yum-epel.git
 git clone git://github.com/opscode-cookbooks/yum.git
 git clone git://github.com/opscode-cookbooks/openssh.git 
 git clone git://github.com/opscode-cookbooks/build-essential.git

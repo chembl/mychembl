9574f0b3ec01d2863c242d8b146e4379379bb47d
mdavies <mdavies@ebi.ac.uk>
2014-05-15 16:29:01 +0100

    Opening links in new tabs on LaunchPad

--- launchpad/footer.php
+++ launchpad/footer.php
@@ -4,7 +4,7 @@
             <p><span>myChEMBL 2014</span>|<span>ChEMBL Release 18</span></p>
           </div>
           <div class="col-lg-6">
-            <p class="text-right"><span><a href="https://www.ebi.ac.uk/chembl/">ChEMBL Website</a></span>|<span><a href="mailto:mychembl@ebi.ac.uk">myChEMBL Support</a></span></p>         
+            <p class="text-right"><span><a href="https://www.ebi.ac.uk/chembl/" target="_blank">ChEMBL Website</a></span>|<span><a href="mailto:mychembl@ebi.ac.uk">myChEMBL Support</a></span></p>         
           </div>            
         </div>
       </row>
--- launchpad/index.php
+++ launchpad/index.php
@@ -7,17 +7,17 @@
 
       <div class="row">
         <div class="col-lg-4">
-          <h3><i class="fa fa-rocket"></i>&nbsp;<a href="chemblws/docs/">ChEMBL Web Services</a></h3>
+          <h3><i class="fa fa-rocket"></i>&nbsp;<a href="chemblws/docs/" target="_blank">ChEMBL Web Services</a></h3>
           <p>Access to a local version of the official ChEMBL Web Services, which connect to the myChEMBL PostgreSQL database.</p>
 
-          <h3><i class="fa fa-rocket"></i>&nbsp;<a href="mychembl/app/home.php">myChEMBL Web Interface</a></h3>
+          <h3><i class="fa fa-rocket"></i>&nbsp;<a href="mychembl/app/home.php" target="_blank">myChEMBL Web Interface</a></h3>
           <p>The myChEMBL Web Interface is a user-friendly application, providing quick access to the myChEMBL data without any prior knowledge of SQL or RDKit.</p>
           
         </div>
 
         <div class="col-lg-4">
             
-          <h3><i class="fa fa-rocket"></i>&nbsp;<a href="phppgadmin">phpPgAdmin Console</a></h3>        
+          <h3><i class="fa fa-rocket"></i>&nbsp;<a href="phppgadmin" target="_blank">phpPgAdmin Console</a></h3>        
           <p>Use the phpPgAdmin console to explore the myChEMBL PostgreSQL database (including RDKit chemical cartridge tables) and run SQL queries. 
             <ul>
               <li><strong>username:</strong> mychembl</li>
@@ -25,23 +25,23 @@
             </ul>
           </p>
 
-          <h3><i class="fa fa-rocket"></i>&nbsp;<a href="knime">KNIME Integration</a></h3>
+          <h3><i class="fa fa-rocket"></i>&nbsp;<a href="knime" target="_blank">KNIME Integration</a></h3>
           <p>Learn how to connect the KNIME workbench to myChEMBL and also how to start processing ChEMBL data within a workflow environment.</p>
                           
         </div>
         
         <div class="col-lg-4">
         
-          <h3><i class="fa fa-rocket"></i>&nbsp;<a href="/" onclick="javascript:event.target.port=8888">IPython Notebooks</a></h3>
+          <h3><i class="fa fa-rocket"></i>&nbsp;<a href="/" onclick="javascript:event.target.port=8888" target="_blank">IPython Notebooks</a></h3>
           <p>A selection of programmatic tutorials written in Python and presented using interactive IPython Notebooks, some examples:</p> 
             <ul>
-              <li><a href="/notebooks/01_myChEMBL_introduction.ipynb" onclick="javascript:event.target.port=8888">myChEMBL introduction</a></li>
-              <li><a href="/notebooks/02_myChEMBL_web_services.ipynb" onclick="javascript:event.target.port=8888">Using myChEMBL Web Services</a></li>
-              <li><a href="/notebooks/03_myChEMBL_predict_targets.ipynb" onclick="javascript:event.target.port=8888">Building predictive target models</a></li>
+              <li><a href="/notebooks/01_myChEMBL_introduction.ipynb" onclick="javascript:event.target.port=8888" target="_blank">myChEMBL introduction</a></li>
+              <li><a href="/notebooks/02_myChEMBL_web_services.ipynb" onclick="javascript:event.target.port=8888" target="_blank">Using myChEMBL Web Services</a></li>
+              <li><a href="/notebooks/03_myChEMBL_predict_targets.ipynb" onclick="javascript:event.target.port=8888" target="_blank">Building predictive target models</a></li>
             </ul>
           </p>
         
-          <h3><i class="fa fa-rocket"></i>&nbsp;<a href="more">More Information</a></h3>
+          <h3><i class="fa fa-rocket"></i>&nbsp;<a href="more" target="_blank">More Information</a></h3>
           <p>For more details on the myChEMBL project, including background, acknowledgements and references.</p>
           
         </div>

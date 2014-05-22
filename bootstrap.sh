7a09d9597acbbe45522046aab2b9905d35ebf92e
Michał Nowotka <mmmnow@gmail.com>
2014-05-22 13:31:08 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install apache2
-sudo apt-get install php5
-sudo apt-get install php5-pgsql
-sudo apt-get install git
-sudo apt-get install unzip
-sudo apt-get install ipython
-sudo apt-get install ipython-notebook
-sudo apt-get install ipython-qtconsole
-sudo apt-get install libboost-all-dev
-sudo apt-get install postgresql
-sudo apt-get install postgresql-server-dev-all
-sudo apt-get install postgresql-doc
-sudo apt-get install postgresql-contrib
-sudo apt-get install flex
-sudo apt-get install bison
-sudo apt-get install g++
-sudo apt-get install cmake
-sudo apt-get install unzip
-sudo apt-get install git
-sudo apt-get install python-numpy
-sudo apt-get install python-scipy
-sudo apt-get install python-matplotlib
-sudo apt-get install python-pip
-sudo apt-get install python-psycopg2
-sudo apt-get install python-imaging-tk
-sudo apt-get install python-pandas
-sudo apt-get install python-networkx
-sudo apt-get install python-sklearn
+sudo apt-get install -y apache2
+sudo apt-get install -y php5
+sudo apt-get install -y php5-pgsql
+sudo apt-get install -y git
+sudo apt-get install -y unzip
+sudo apt-get install -y ipython
+sudo apt-get install -y ipython-notebook
+sudo apt-get install -y ipython-qtconsole
+sudo apt-get install -y libboost-all-dev
+sudo apt-get install -y postgresql
+sudo apt-get install -y postgresql-server-dev-all
+sudo apt-get install -y postgresql-doc
+sudo apt-get install -y postgresql-contrib
+sudo apt-get install -y flex
+sudo apt-get install -y bison
+sudo apt-get install -y g++
+sudo apt-get install -y cmake
+sudo apt-get install -y unzip
+sudo apt-get install -y git
+sudo apt-get install -y python-numpy
+sudo apt-get install -y python-scipy
+sudo apt-get install -y python-matplotlib
+sudo apt-get install -y python-pip
+sudo apt-get install -y python-psycopg2
+sudo apt-get install -y python-imaging-tk
+sudo apt-get install -y python-pandas
+sudo apt-get install -y python-networkx
+sudo apt-get install -y python-sklearn
 
 sudo pip install -U ipython
 sudo pip install -U jinja2

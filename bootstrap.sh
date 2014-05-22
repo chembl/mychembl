b6fc5f3adc48c95edbbd036cb2f10e701cf877d9
Michał Nowotka <mmmnow@gmail.com>
2014-05-22 16:11:25 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -41,14 +41,12 @@ sudo pip install -U ipython
 sudo pip install -U jinja2
 sudo pip install -U scikit-learn
 sudo pip install -U tornado
-sudo pip install -U pandas #will update numpy too
+sudo pip install -U pandas
 sudo pip install -U requests
 
-cd /home/chembl
-mkdir src
-cd src
-sudo git clone https://github.com/chembl/mychembl_webapp.git
-sudo git clone https://github.com/chembl/mychembl.git
+sudo -u chembl mkdir /home/chembl/src
+sudo -u chembl git clone https://github.com/chembl/mychembl_webapp.git /home/chembl/src
+sudo -u chembl git clone https://github.com/chembl/mychembl.git /home/chembl/src
 sudo mv /var/www /var/www_old
 sudo mkdir /var/www
 sudo cp -r mychembl/launchpad/* /var/www/
@@ -59,5 +57,5 @@ unzip JSME_2013-08-04.zip
 sudo mv JSME_2013-08-04/jsme /var/www/mychembl/static/js/
 sudo cp mychembl/configuration/launchpad_httpd.conf /etc/apache2/httpd.conf
 
-curl -s https://raw.githubusercontent.com/chembl/mychembl/master/webservices/ws_setup.sh | sh
-curl -s https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/ipynb_deamonise.sh | sh
+curl -s https://raw.githubusercontent.com/chembl/mychembl/master/webservices/ws_setup.sh | sudo -i -u chembl sh
+curl -s https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/ipynb_deamonise.sh | sudo -i -u chembl sh

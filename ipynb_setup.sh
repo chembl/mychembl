134c01f9420a09a24ed081c83b5470230b9b9128
Michał Nowotka <mmmnow@gmail.com>
2014-05-23 12:20:57 +0200

    Update ipynb_setup.sh

--- ipynb_setup.sh
+++ ipynb_setup.sh
@@ -2,6 +2,4 @@
 
 mkdir /home/chembl/notebooks
 ipython profile create mychembl
-ipython notebook --profile=mychembl
-
 curl -o /home/chembl/.ipython/profile_mychembl/ipython_notebook_config.py https://raw.githubusercontent.com/chembl/mychembl/master/configuration/mychembl_ipython_notebook_config.py

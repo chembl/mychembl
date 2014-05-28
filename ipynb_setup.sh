f1db6497796470906f1f9eb54999b022dee98d8c
Michał Nowotka <mmmnow@gmail.com>
2014-05-28 15:14:31 +0200

    Update ipynb_setup.sh

--- ipynb_setup.sh
+++ ipynb_setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 mkdir /home/chembl/notebooks
+curl -o /home/chembl/notebooks/01_myChEMBL_introduction.ipynb https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/01_myChEMBL_introduction.ipynb
+curl -o /home/chembl/notebooks/02_myChEMBL_web_services.ipynb https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/02_myChEMBL_web_services.ipynb
+curl -o /home/chembl/notebooks/03_myChEMBL_predict_targets.ipynb https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/03_myChEMBL_predict_targets.ipynb
+curl -o /home/chembl/notebooks/04_myChEMBL_plotting_tutorial.ipynb https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/04_myChEMBL_plotting_tutorial.ipynb
+curl -o /home/chembl/notebooks/05_myChEMBL_mds_tutorial.ipynb https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/05_myChEMBL_mds_tutorial.ipynb
+curl -o /home/chembl/notebooks/06_Differences_between_ChEMBL_and_myChEMBL.ipynb https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/06_Differences_between_ChEMBL_and_myChEMBL.ipynb
+
 ipython profile create mychembl
 curl -o /home/chembl/.ipython/profile_mychembl/ipython_notebook_config.py https://raw.githubusercontent.com/chembl/mychembl/master/configuration/mychembl_ipython_notebook_config.py

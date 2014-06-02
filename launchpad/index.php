5d5dd6e8b8f38dc7cc9ea4601590749eb21c953a
mdavies <mdavies@ebi.ac.uk>
2014-06-02 14:18:11 +0100

    LaunchPad links reorder

--- launchpad/index.php
+++ launchpad/index.php
@@ -6,34 +6,41 @@
       </div>
 
       <div class="row">
+        
         <div class="col-lg-4">
-          <h3><i class="fa fa-rocket"></i>&nbsp;<a href="chemblws/docs/" target="_blank">Web Services</a></h3>
-          <p>Access to a local version of the official ChEMBL Web Services, which connect to the myChEMBL PostgreSQL database.</p>
-
           <h3><i class="fa fa-rocket"></i>&nbsp;<a href="mychembl/app/home.php" target="_blank">Web Interface</a></h3>
           <p>This web interface provides quick access to the myChEMBL data without any prior knowledge of SQL or RDKit.</p>
-          
+        </div>
+       
+        <div class="col-lg-4">      
+          <h3><i class="fa fa-rocket"></i>&nbsp;<a href="phppgadmin" target="_blank">phpPgAdmin Console</a></h3>        
+          <p>Use the phpPgAdmin console to explore the myChEMBL PostgreSQL database and run SQL queries (<strong>user:</strong> mychembl, <strong>password:</strong> read).</p>        
+        </div>
+        
+        <div class="col-lg-4">
+          <h3><i class="fa fa-rocket"></i>&nbsp;<a href="chemblws/docs/" target="_blank">Web Services</a></h3>
+          <p>Access to a local version of the official ChEMBL Web Services, which connect to the myChEMBL PostgreSQL database.</p>     
+        </div>
+      
+      </div>
+ 
+      <div class="row">
+        
+        <div class="col-lg-4">
+          <h3><i class="fa fa-rocket"></i>&nbsp;<a href="/" onclick="javascript:event.target.port=8888" target="_blank">IPython Notebooks</a></h3>
+          <p>A selection of programmatic tutorials written in Python and presented using interactive IPython Notebooks.</p> 
         </div>
 
         <div class="col-lg-4">
-            
-          <h3><i class="fa fa-rocket"></i>&nbsp;<a href="phppgadmin" target="_blank">phpPgAdmin Console</a></h3>        
-          <p>Use the phpPgAdmin console to explore the myChEMBL PostgreSQL database and run SQL queries (<strong>user:</strong> mychembl, <strong>password:</strong> read).</p>
-
           <h3><i class="fa fa-rocket"></i>&nbsp;<a href="knime" target="_blank">KNIME Integration</a></h3>
           <p>Learn how to connect the KNIME workbench to myChEMBL and also how to start processing ChEMBL data within a workflow environment.</p>
-                          
         </div>
         
         <div class="col-lg-4">
-        
-          <h3><i class="fa fa-rocket"></i>&nbsp;<a href="/" onclick="javascript:event.target.port=8888" target="_blank">IPython Notebooks</a></h3>
-          <p>A selection of programmatic tutorials written in Python and presented using interactive IPython Notebooks.</p>
-        
           <h3><i class="fa fa-rocket"></i>&nbsp;<a href="more" target="_blank">More Information</a></h3>
           <p>For more details on the myChEMBL project, including background, acknowledgements and references.</p>
-          
         </div>
+
       </div>
 
 <? include "footer.php"; ?>

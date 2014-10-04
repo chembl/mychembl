97edcea2936d668bf1324e7a2b1321716cb56b77
Michał Nowotka <mmmnow@gmail.com>
2014-10-04 12:10:32 +0100

    Update osra.sh

--- osra.sh
+++ osra.sh
@@ -8,13 +8,22 @@ sudo apt-get install -y libgraphicsmagick3
 sudo apt-get install -y libpotrace-dev
 sudo apt-get install -y libpotrace0
 sudo apt-get install -y potrace
-sudo apt-get install -y libocrad-dev
-sudo apt-get install -y ocrad
 sudo apt-get install -y libtclap-dev
 sudo apt-get install -y openbabel
 sudo apt-get install -y libopenbabel-dev
 sudo apt-get install -y libnetpbm10-dev
 sudo apt-get install -y libglib2.0-dev
+
+cd /tmp
+wget http://ftp.heanet.ie/mirrors/gnu/ocrad/ocrad-0.21.tar.gz -O ocrad.tgz
+tar -zxvf ocrad.tgz
+cd ocrad-0.21/
+./configure
+make
+sudo make install
+cd ..
+rm -rf ocrad-0.21/
+rm ocrad.tgz
 
 cd /tmp
 wget http://sourceforge.net/projects/osra/files/gocr-patched/gocr-0.50pre-patched.tgz
@@ -23,14 +32,20 @@ cd gocr-0.50pre-patched/
 ./configure
 make libs
 sudo make install
+cd ..
+rm -rf gocr-0.50pre-patched/
+rm gocr-0.50pre-patched.tgz
 
 cd /tmp
 wget sourceforge.net/projects/osra/files/osra/2.0.0/osra-2.0.0.tgz
 tar -zxvf osra-2.0.0.tgz
 cd osra-2.0.0/
-./configure --with-gocr-include=/tmp/gocr-0.50pre-patched/src
-
+./configure
+make
+sudo make install
 cd ..
-rm -rf gocr-0.50pre-patched/
-rm gocr-0.50pre-patched.tgz
+rm -rf osra-2.0.0/
+rm osra-2.0.0.tgz
+
+
 

6e9b1942cff00812c9a0fcf57a770eeff7613a9a
Michał Nowotka <mmmnow@gmail.com>
2014-10-06 10:20:19 +0100

    Update install_beaker.sh

---
+++ beaker/install_beaker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script installs and configures local copy of Beaker on
+# myChEMBL VM.
+# Author: Michal Nowotka, mnowotka@ebi.ac.uk
+# Created date: 06.10.2014
+
+# First, create a new virtualenv called chembl_beaker
+mkvirtualenv chembl_beaker
+
+#installing beaker:
+pip install chembl_beaker

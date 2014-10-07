923b571100cf0efce2f1dbbb594ebeffb850739d
Michał Nowotka <mmmnow@gmail.com>
2014-10-07 10:10:08 +0100

    Update osra.sh

--- osra.sh
+++ osra.sh
@@ -5,19 +5,19 @@
 # Created date: 06.10.2014
 
 # first, let's install all required dependencies, that can be installed from ubuntu repositories
-yes | sudo apt-get install -y graphicsmagick
-yes | sudo apt-get install -y libgraphicsmagick++1-dev
-yes | sudo apt-get install -y libgraphicsmagick++3
-yes | sudo apt-get install -y libgraphicsmagick1-dev
-yes | sudo apt-get install -y libgraphicsmagick3
-yes | sudo apt-get install -y libpotrace-dev
-yes | sudo apt-get install -y libpotrace0
-yes | sudo apt-get install -y potrace
-yes | sudo apt-get install -y libtclap-dev
-yes | sudo apt-get install -y openbabel
-yes | sudo apt-get install -y libopenbabel-dev
-yes | sudo apt-get install -y libnetpbm10-dev
-yes | sudo apt-get install -y libglib2.0-dev
+yes | sudo apt-get install graphicsmagick
+yes | sudo apt-get install libgraphicsmagick++1-dev
+yes | sudo apt-get install libgraphicsmagick++3
+yes | sudo apt-get install libgraphicsmagick1-dev
+yes | sudo apt-get install libgraphicsmagick3
+yes | sudo apt-get install libpotrace-dev
+yes | sudo apt-get install libpotrace0
+yes | sudo apt-get install potrace
+yes | sudo apt-get install libtclap-dev
+yes | sudo apt-get install openbabel
+yes | sudo apt-get install libopenbabel-dev
+yes | sudo apt-get install libnetpbm10-dev
+yes | sudo apt-get install libglib2.0-dev
 
 # installing ocrad (https://www.gnu.org/software/ocrad/) from source bacause standard distribution doesn't work for me...
 cd /tmp

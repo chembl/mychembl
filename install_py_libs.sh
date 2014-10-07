f138abaf13fcb4b12940f87d0c3b2ae13ebb2870
Michał Nowotka <mmmnow@gmail.com>
2014-10-07 14:03:12 +0100

    Create install_py_libs.sh

---
+++ install_py_libs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo pip install Cython
+sudo pip install -U ipython
+sudo pip install -U jinja2
+sudo pip install -U scikit-learn
+sudo pip install -U tornado
+sudo pip install -U pandas
+#sudo pip install -U requests
+sudo pip install -U mpld3
+sudo pip install -U service_identity
+sudo pip install https://garage.maemo.org/frs/download.php/8363/python-brisa_0.10.3maemo0.tar.gz
+sudo pip install -U paste
+sudo pip install -U netifaces

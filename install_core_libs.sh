4350c8b4cbc94edfa85c16da88f6e17d9c64660d
Michał Nowotka <mmmnow@gmail.com>
2014-10-07 13:59:52 +0100

    Create install_core_libs.sh

---
+++ install_core_libs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y apache2
+sudo apt-get install -y php5
+sudo apt-get install -y php5-pgsql
+sudo apt-get install -y git
+sudo apt-get install -y unzip
+sudo apt-get install -y ipython
+sudo apt-get install -y ipython-notebook
+sudo apt-get install -y ipython-qtconsole
+sudo apt-get install -y libboost-all-dev
+sudo apt-get install -y postgresql
+sudo apt-get install -y postgresql-server-dev-all
+sudo apt-get install -y postgresql-doc
+sudo apt-get install -y postgresql-contrib
+sudo apt-get install -y flex
+sudo apt-get install -y bison
+sudo apt-get install -y g++
+sudo apt-get install -y cmake
+sudo apt-get install -y libffi-dev
+sudo apt-get install -y python-numpy
+sudo apt-get install -y python-scipy
+sudo apt-get install -y python-matplotlib
+sudo apt-get install -y python-pip
+sudo apt-get install -y python-psycopg2
+sudo apt-get install -y python-imaging-tk
+#sudo apt-get install -y python-pandas
+sudo apt-get install -y python-networkx
+#sudo apt-get install -y python-sklearn
+sudo apt-get install -y libnss-mdns
+sudo apt-get install -y avahi-utils
+sudo apt-get install -y python-gobject
+sudo apt-get install -y python-dev
+sudo apt-get install -y phppgadmin

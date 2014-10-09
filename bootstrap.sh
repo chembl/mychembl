98471389cd7812a17857839e87ed3010df5462ae
Mark Davies <markdd1@gmail.com>
2014-10-10 00:14:53 +0100

    Copying .htaccess

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,6 +23,7 @@ sudo git clone https://github.com/chembl/mychembl.git
 sudo rm /var/www -rf
 sudo mkdir -p /var/www/html/mychembl
 sudo cp -r mychembl/launchpad/* /var/www/html/
+sudo cp -r mychembl/launchpad/.htaccess /var/www/html/
 sudo cp -r mychembl_webapp/* /var/www/html/mychembl/
 curl -O http://peter-ertl.com/jsme/download/JSME_2013-08-04.zip
 unzip JSME_2013-08-04.zip

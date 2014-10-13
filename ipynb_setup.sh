74ee9e5887e1d6bbb4317caff859678874faba87
mdavies <mdavies@ebi.ac.uk>
2014-10-13 22:42:24 +0100

    Destination file name chang

--- ipynb_setup.sh
+++ ipynb_setup.sh
@@ -9,7 +9,7 @@ curl -o /home/chembl/notebooks/05_myChEMBL_mds_tutorial.ipynb https://raw.github
 curl -o /home/chembl/notebooks/06_myChEMBL_differences_with_ChEMBL.ipynb https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/06_myChEMBL_differences_with_ChEMBL.ipynb
 curl -o /home/chembl/notebooks/07_myChEMBL_using_rdkit_without_rdkit.ipynb https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/07_myChEMBL_using_rdkit_without_rdkit.ipynb
 curl -o /home/chembl/notebooks/08_myChEMBL_using_django_ORM_to_access_ChEMBL_without_SQL.ipynb https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/08_myChEMBL_using_django_ORM_to_access_ChEMBL_without_SQL.ipynb
-curl -o /home/chembl/notebooks/09_myChEMBL_blast.ipynb https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/09_myChEMBL_BLAST_and_Druggability.ipynb
+curl -o /home/chembl/notebooks/09_myChEMBL_BLAST_and_Druggability.ipynb https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/09_myChEMBL_BLAST_and_Druggability.ipynb
 
 ipython profile create mychembl
 curl -o /home/chembl/.ipython/profile_mychembl/ipython_notebook_config.py https://raw.githubusercontent.com/chembl/mychembl/master/configuration/mychembl_ipython_notebook_config.py

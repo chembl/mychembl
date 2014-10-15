bffa8f66b006fc7f00a352ecb21ad61adc338c55
George Papadatos <gpapadatos@gmail.com>
2014-10-15 14:18:27 +0100

    new data files

--- ipython_notebooks/ipynb_deamonise.sh
+++ ipython_notebooks/ipynb_deamonise.sh
@@ -22,6 +22,12 @@ touch /home/chembl/ipynb_workbench/__init__.py
 # install some files to ipynb_workbench:
 curl -o /home/chembl/ipynb_workbench/physchem_props_heatmap.csv https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/physchem_props_heatmap.csv
 curl -o /home/chembl/ipynb_workbench/settings.py https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/settings.py
+curl -o /home/chembl/ipynb_workbench/A2A_Adenosine_set_mychembl.csv https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/A2A_Adenosine_set_mychembl.csv
+curl -o /home/chembl/ipynb_workbench/US_antimalarial_patents_cmpds.txt https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/US_antimalarial_patents_cmpds.txt
+curl -o /home/chembl/ipynb_workbench/fps.pkl https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/fps.pkl
+curl -o /home/chembl/ipynb_workbench/smiles_cas_N6512.smi https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/smiles_cas_N6512.smi
+
+
 
 # Configure it to take care about ipython notebook server:
 echo "chemblvm" | sudo -S curl -o /etc/supervisor/conf.d/ipynb.conf https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/ipynb.conf

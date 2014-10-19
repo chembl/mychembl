b123e32d7cad7c215bd2a0d2dc5dc492c15934dd
Michał Nowotka <mmmnow@gmail.com>
2014-10-19 17:12:16 +0100

    Update install_core_libs.sh

--- install_core_libs.sh
+++ install_core_libs.sh
@@ -2,9 +2,6 @@
 
 sudo apt-get update
 sudo apt-get install -y bash
-sudo apt-get install -y apache2
-sudo apt-get install -y php5
-sudo apt-get install -y php5-pgsql
 sudo apt-get install -y git
 sudo apt-get install -y unzip
 sudo apt-get install -y ipython
@@ -21,20 +18,19 @@ sudo apt-get install -y g++
 sudo apt-get install -y cmake
 sudo apt-get install -y make
 sudo apt-get install -y libffi-dev
+sudo apt-get install -y libxml2 libxml2-dev
+sudo apt-get install -y libxslt1.1 libxslt1-dev
 sudo apt-get install -y python-numpy
 sudo apt-get install -y python-scipy
 sudo apt-get install -y python-matplotlib
 sudo apt-get install -y python-pip
 sudo apt-get install -y python-psycopg2
 sudo apt-get install -y python-imaging-tk
-#sudo apt-get install -y python-pandas
 sudo apt-get install -y python-networkx
-#sudo apt-get install -y python-sklearn
 sudo apt-get install -y libnss-mdns
 sudo apt-get install -y avahi-utils
 sudo apt-get install -y python-gobject
 sudo apt-get install -y python-dev
-sudo apt-get install -y phppgadmin
 sudo apt-get install -y python-biopython
 
 gem install gist

40e4f734f70a4d9a6901439f4aeba8fc86b32e14
Michał Nowotka <mmmnow@gmail.com>
2014-10-19 21:49:27 +0100

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -10,6 +10,6 @@ sudo curl -o /etc/phppgadmin/apache.conf https://raw.githubusercontent.com/chemb
 sudo curl -o /etc/apache2/conf.d/phppgadmin https://raw.githubusercontent.com/chembl/mychembl/master/configuration/mychembl_phppgadmin_httpd.conf
 sudo curl -o /etc/php5/apache2/php.ini https://raw.githubusercontent.com/chembl/mychembl/master/configuration/mychembl_php.ini
 sudo a2enmod rewrite
-sudo apache2ctl restart
+sudo service apache2 restart
 
 wget https://raw.githubusercontent.com/chembl/mychembl/master/webservices/ws_cache_generation.sh && sh ws_cache_generation.sh

667f0b1b35512a598c734383a5fed312694a17fe
Michał Nowotka <mmmnow@gmail.com>
2015-05-21 17:01:15 +0100

    Create install_core_libs_centOS.sh

---
+++ install_core_libs_centOS.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# for  CentOS Linux release 7.1.1503
+
+sudo rpm -iUvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+sudo yum update
+sudo yum install -y bash
+sudo yum install -y git
+sudo yum install -y unzip
+#sudo apt-get install -y ipython
+#sudo apt-get install -y ipython-notebook
+#sudo apt-get install -y ipython-qtconsole
+sudo yum install -y boost
+sudo yum install -y postgresql
+sudo yum install -y postgresql-devel
+sudo yum install -y postgresql-docs
+sudo yum install -y postgresql-contrib
+sudo yum install -y flex
+sudo yum install -y bison
+sudo yum install -y gcc-c++
+sudo yum install -y cmake
+sudo yum install -y make
+sudo yum install -y libffi-devel
+sudo yum install -y libxml2 libxml2-devel
+sudo yum install -y libxslt libxslt-devel
+sudo yum install -y numpy
+sudo yum install -y scipy
+sudo yum install -y python-matplotlib
+sudo yum install -y python-pip
+sudo yum install -y python-psycopg2
+#sudo apt-get install -y python-imaging-tk
+sudo yum install -y python-networkx
+sudo yum install -y nss-mdns avahi-compat-libdns_sd-devel
+sudo yum install -y avahi-tools
+sudo yum install -y pygobject3
+sudo yum install -y python-devel
+#sudo apt-get install -y python-biopython
+#sudo apt-get install -y rcconf
+
+#gem install gist

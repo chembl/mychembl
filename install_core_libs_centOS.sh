1812b8f2639d52e726a592fc285620f1a4b4d021
Michał Nowotka <mmmnow@gmail.com>
2015-05-26 13:01:55 +0100

    Update install_core_libs_centOS.sh

--- install_core_libs_centOS.sh
+++ install_core_libs_centOS.sh
@@ -34,7 +34,7 @@ sudo yum -y --enablerepo=epel install uuid
 sudo yum install -y postgresql93 postgresql93-server postgresql93-contrib postgresql93-libs --disablerepo=* --enablerepo=pgdg93
 #sudo yum install -y python-psycopg2
 sudo yum install -y tkinter
-sudo yum install -y python-networkx
+#sudo yum install -y python-networkx
 sudo yum install -y nss-mdns avahi-compat-libdns_sd-devel
 sudo yum install -y avahi-tools
 sudo yum install -y pygobject3

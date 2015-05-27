e6e1c30515be555691f655ea4eae6c21a499dbc3
Michał Nowotka <mmmnow@gmail.com>
2015-05-27 14:31:15 +0100

    Update install_core_libs_centOS.sh

--- install_core_libs_centOS.sh
+++ install_core_libs_centOS.sh
@@ -16,28 +16,18 @@ sudo yum install -y ruby
 sudo yum install -y openssl-devel
 sudo yum install -y libxml2 libxml2-devel
 sudo yum install -y libxslt libxslt-devel
-#sudo apt-get install -y ipython-qtconsole
 sudo yum install -y boost
 sudo yum install -y boost-devel
-#sudo yum install -y postgresql
-#sudo yum install -y postgresql-devel
-#sudo yum install -y postgresql-docs
-#sudo yum install -y postgresql-contrib
 sudo yum install -y flex
 sudo yum install -y bison
 sudo yum install -y gcc*
 sudo yum install -y cmake
 sudo yum install -y make
 sudo yum install -y libffi-devel
-#sudo yum install -y numpy
-#sudo yum install -y scipy
-#sudo yum install -y python-matplotlib
-#sudo yum install -y python-pip
 sudo yum -y --enablerepo=epel install uuid
 sudo yum install -y postgresql93 postgresql93-server postgresql93-contrib postgresql93-libs --disablerepo=* --enablerepo=pgdg93
 sudo yum install -y python-psycopg2
 sudo yum install -y tkinter
-#sudo yum install -y python-networkx
 sudo yum install -y nss-mdns avahi-compat-libdns_sd-devel
 sudo yum install -y avahi-tools
 sudo yum install -y pygobject3

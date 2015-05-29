3b1d24cd35f21d2ecdb27e8835879498b1161f1a
Michał Nowotka <mmmnow@gmail.com>
2015-05-29 13:47:32 +0100

    Update install_py_libs.sh

--- install_py_libs.sh
+++ install_py_libs.sh
@@ -6,6 +6,8 @@ sudo -H python get-pip.py
 sudo -H pip install pyopenssl 
 sudo -H pip install ndg-httpsclient 
 sudo -H pip install pyasn1
+sudo -H pip install lxml
+sudo -H pip install supervisor --pre
 sudo -H pip install Pillow
 sudo -H pip install numpy
 sudo -H pip install matplotlib
@@ -16,7 +18,6 @@ sudo -H pip install -U scikit-learn==0.14.1
 sudo -H pip install -U chembl_migration_model
 echo "numpy==1.8.2" | cat > /tmp/pandas.requirement.txt
 sudo -H pip install -U pandas -r /tmp/pandas.requirement.txt
-#sudo pip install -U requests
 sudo -H pip install -U mpld3
 sudo -H pip install -U service_identity
 sudo -H pip install https://garage.maemo.org/frs/download.php/8363/python-brisa_0.10.3maemo0.tar.gz

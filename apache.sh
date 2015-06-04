61ea5723682abe497f6444d12160caab27a39d25
Michał Nowotka <mmmnow@gmail.com>
2015-06-04 11:29:49 +0100

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -3,7 +3,7 @@ python -mplatform | grep Ubuntu && export APACHE_ENV_FILE=/etc/apache2/apache2.c
 RAW=https://raw.githubusercontent.com/chembl/mychembl/master
 
 sudo curl -o $APACHE_HOME/conf.d/launchpad.conf $RAW/configuration/launchpad.conf
-sudo echo "export LD_LIBRARY_PATH=/home/chembl/rdkit/lib:$LD_LIBRARY_PATH" >> $APACHE_ENV_FILE
+sudo bash -c 'echo "export LD_LIBRARY_PATH=/home/chembl/rdkit/lib:$LD_LIBRARY_PATH" >> $APACHE_ENV_FILE'
 sudo curl -o /etc/phppgadmin/apache.conf $RAW/configuration/mychembl_phppgadmin_httpd.conf
 sudo curl -o /etc/apache2/conf.d/phppgadmin $RAW/configuration/mychembl_phppgadmin_httpd.conf
 sudo curl -o /etc/php5/apache2/php.ini $RAW/configuration/mychembl_php.ini

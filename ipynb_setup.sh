df67b1585b2e86c934ebf6f39fcf9ee0072f55e8
Michał Nowotka <mmmnow@gmail.com>
2015-06-10 17:30:54 +0100

    Update ipynb_setup.sh

--- ipynb_setup.sh
+++ ipynb_setup.sh
@@ -1,40 +1,45 @@
 #!/bin/bash
 
-mkdir /home/chembl/notebooks
-curl -o /home/chembl/notebooks/01_myChEMBL_introduction.ipynb https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/01_myChEMBL_introduction.ipynb
-curl -o /home/chembl/notebooks/02_myChEMBL_web_services.ipynb https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/02_myChEMBL_web_services.ipynb
-curl -o /home/chembl/notebooks/03_myChEMBL_predict_targets.ipynb https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/03_myChEMBL_predict_targets.ipynb
-curl -o /home/chembl/notebooks/04_myChEMBL_plotting_tutorial.ipynb https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/04_myChEMBL_plotting_tutorial.ipynb
-curl -o /home/chembl/notebooks/05_myChEMBL_MDS_tutorial.ipynb https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/05_myChEMBL_mds_tutorial.ipynb
-curl -o /home/chembl/notebooks/06_myChEMBL_differences_with_ChEMBL.ipynb https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/06_myChEMBL_differences_with_ChEMBL.ipynb
-curl -o /home/chembl/notebooks/07_myChEMBL_Beaker.ipynb https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/07_myChEMBL_Beaker.ipynb
-curl -o /home/chembl/notebooks/08_myChEMBL_Django_ORM.ipynb https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/08_myChEMBL_Django_ORM.ipynb
-curl -o /home/chembl/notebooks/09_myChEMBL_BLAST_and_druggability.ipynb https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/09_myChEMBL_BLAST_and_druggability.ipynb
-curl -o /home/chembl/notebooks/10_myChEMBL_machine_learning.ipynb https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/10_myChEMBL_machine_learning.ipynb
-curl -o /home/chembl/notebooks/11_myChEMBL_SureChEMBL.ipynb https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/11_myChEMBL_SureChEMBL.ipynb
-curl -o /home/chembl/notebooks/12_myChEMBL_drugs_ADME.ipynb https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/12_myChEMBL_drugs_ADME.ipynb
+export HOME="/home/chembl"
+NOTEBOOKS="$HOME/notebooks"
+WORKBENCH="$HOME/ipynb_workbench"
+I_NOTEBOOKS="$RAW/ipython_notebooks"
+
+mkdir $NOTEBOOKS
+curl -o $NOTEBOOKS/01_myChEMBL_introduction.ipynb $I_NOTEBOOKS/01_myChEMBL_introduction.ipynb
+curl -o $NOTEBOOKS/02_myChEMBL_web_services.ipynb $I_NOTEBOOKS/02_myChEMBL_web_services.ipynb
+curl -o $NOTEBOOKS/03_myChEMBL_predict_targets.ipynb $I_NOTEBOOKS/03_myChEMBL_predict_targets.ipynb
+curl -o $NOTEBOOKS/04_myChEMBL_plotting_tutorial.ipynb $I_NOTEBOOKS/04_myChEMBL_plotting_tutorial.ipynb
+curl -o $NOTEBOOKS/05_myChEMBL_MDS_tutorial.ipynb $I_NOTEBOOKS/05_myChEMBL_mds_tutorial.ipynb
+curl -o $NOTEBOOKS/06_myChEMBL_differences_with_ChEMBL.ipynb $I_NOTEBOOKS/06_myChEMBL_differences_with_ChEMBL.ipynb
+curl -o $NOTEBOOKS/07_myChEMBL_Beaker.ipynb $I_NOTEBOOKS/07_myChEMBL_Beaker.ipynb
+curl -o $NOTEBOOKS/08_myChEMBL_Django_ORM.ipynb $I_NOTEBOOKS/08_myChEMBL_Django_ORM.ipynb
+curl -o $NOTEBOOKS/09_myChEMBL_BLAST_and_druggability.ipynb $I_NOTEBOOKS/09_myChEMBL_BLAST_and_druggability.ipynb
+curl -o $NOTEBOOKS/10_myChEMBL_machine_learning.ipynb $I_NOTEBOOKS/10_myChEMBL_machine_learning.ipynb
+curl -o $NOTEBOOKS/11_myChEMBL_SureChEMBL.ipynb $I_NOTEBOOKS/11_myChEMBL_SureChEMBL.ipynb
+curl -o $NOTEBOOKS/12_myChEMBL_drugs_ADME.ipynb $I_NOTEBOOKS/12_myChEMBL_drugs_ADME.ipynb
 
 ipython profile create mychembl
-curl -o /home/chembl/.ipython/profile_mychembl/ipython_notebook_config.py https://raw.githubusercontent.com/chembl/mychembl/master/configuration/mychembl_ipython_notebook_config.py
+curl -o $HOME/.ipython/profile_mychembl/ipython_notebook_config.py $RAW/configuration/mychembl_ipython_notebook_config.py
 
 cd /home/chembl/.ipython/nbextensions/
-wget https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js
+wget $RAPHAEL_JS_LOCATION
 mkdir mathjax
 cd mathjax
-wget https://cdn.mathjax.org/mathjax/latest/MathJax.js
+wget $MATHJAX_LOCATION
 
 # create directory for storing temporary files:
-mkdir /home/chembl/ipynb_workbench
+mkdir $WORKBENCH
 
 # we would like to tread ipynb_workbench directory as a regular python module so creating __init__.py:
-touch /home/chembl/ipynb_workbench/__init__.py
+touch $WORKBENCH/__init__.py
 
 # install some files to ipynb_workbench:
-curl -o /home/chembl/ipynb_workbench/physchem_props_heatmap.csv https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/physchem_props_heatmap.csv
-curl -o /home/chembl/ipynb_workbench/settings.py https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/settings.py
-curl -o /home/chembl/ipynb_workbench/A2A_Adenosine_set_mychembl.csv https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/A2A_Adenosine_set_mychembl.csv
-curl -o /home/chembl/ipynb_workbench/US_antimalarial_patents_cmpds.txt https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/US_antimalarial_patents_cmpds.txt
-curl -o /home/chembl/ipynb_workbench/fps.pkl https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/fps.pkl
-curl -o /home/chembl/ipynb_workbench/smiles_cas_N6512.smi https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/smiles_cas_N6512.smi
+curl -o $WORKBENCH/physchem_props_heatmap.csv $I_NOTEBOOKS/physchem_props_heatmap.csv
+curl -o $WORKBENCH/settings.py $I_NOTEBOOKS/settings.py
+curl -o $WORKBENCH/A2A_Adenosine_set_mychembl.csv $I_NOTEBOOKS/A2A_Adenosine_set_mychembl.csv
+curl -o $WORKBENCH/US_antimalarial_patents_cmpds.txt $I_NOTEBOOKS/US_antimalarial_patents_cmpds.txt
+curl -o $WORKBENCH/fps.pkl $I_NOTEBOOKS/fps.pkl
+curl -o $WORKBENCH/smiles_cas_N6512.smi $I_NOTEBOOKS/smiles_cas_N6512.smi
 
-curl -o /home/chembl/.ipython/profile_mychembl/static/custom/custom.css https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/custom.css
+curl -o /home/chembl/.ipython/profile_mychembl/static/custom/custom.css $I_NOTEBOOKS/custom.css

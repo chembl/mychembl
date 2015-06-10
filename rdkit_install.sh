03088e8c9484d836ead53815d3c8c5a3cdd52cc0
Michał Nowotka <mmmnow@gmail.com>
2015-06-10 17:16:56 +0100

    Update rdkit_install.sh

--- rdkit_install.sh
+++ rdkit_install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-export RDBASE=$HOME/rdkit
-export LD_LIBRARY_PATH=$RDBASE/lib:$LD_LIBRARY_PATH
-export PYTHONPATH=$RDBASE:$PYTHONPATH
+HOME=/home/chembl
+RDBASE=$HOME/rdkit
+LD_LIBRARY_PATH=$RDBASE/lib:$LD_LIBRARY_PATH
+PYTHONPATH=$RDBASE:$PYTHONPATH
 python -mplatform | grep Ubuntu && PATH=$PATH || export PATH=$PATH:/usr/pgsql-9.3/bin/
 
 cd $HOME
-git clone https://github.com/rdkit/rdkit
+git clone $RDKIT_REPO
 cd rdkit/
-git checkout tags/Release_2015_03_1
+git checkout tags/$RDKIT_RELEASE
 cd $RDBASE/External/INCHI-API
 sed -i 's/sites\/default\/files\/inchi-1.04\/INCHI-1-API\.ZIP/wp\/wp-content\/uploads\/2014\/06\/INCHI-1-API\.zip/g' download-inchi.sh
 bash download-inchi.sh

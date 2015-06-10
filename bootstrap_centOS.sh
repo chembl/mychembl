3a24ff889b57e349d182be167f40c9f8fb1350da
Michał Nowotka <mmmnow@gmail.com>
2015-06-10 17:39:52 +0100

    Update bootstrap_centOS.sh

--- bootstrap_centOS.sh
+++ bootstrap_centOS.sh
@@ -40,7 +40,7 @@ cd /tmp
 wget $RAW/osra.sh && source osra.sh
 wget $RAW/rdkit_install.sh && sudo -E su -m -c "bash rdkit_install.sh" chembl
 wget $RAW/ipynb_setup.sh && sudo -E su -m -c "bash ipynb_setup.sh" chembl
-wget $RAW/ipython_notebooks/ipynb_deamonise.sh && bash ipynb_deamonise.sh
+wget $RAW/ipython_notebooks/ipynb_deamonise.sh && source ipynb_deamonise.sh
 wget $RAW/create_db.sh && sudo su -c "bash create_db.sh" chembl
 wget $RAW/webservices/ws_setup.sh && sudo su -c "bash ws_setup.sh" chembl
 wget $RAW/beaker/install_beaker.sh && sudo su -c "bash install_beaker.sh" chembl

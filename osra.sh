162381f3b937f30d9885385800f2d0007d435f02
Michał Nowotka <mmmnow@gmail.com>
2015-06-10 17:10:55 +0100

    Update osra.sh

--- osra.sh
+++ osra.sh
@@ -6,48 +6,48 @@
 
 # installing TCLAP (http://tclap.sourceforge.net/)
 cd /tmp
-wget http://downloads.sourceforge.net/project/tclap/tclap-1.2.1.tar.gz -O tclap.tgz
+wget $TCLAP_LOCATION -O tclap.tgz
 tar -zxvf tclap.tgz
-cd tclap-1.2.1/
+cd tclap-${TCLAP_VERSION}/
 ./configure
 make
 sudo make install
 cd ..
-rm -rf tclap-1.2.1/
+rm -rf tclap-${TCLAP_VERSION}/
 rm tclap.tgz
 
 # installing ocrad (https://www.gnu.org/software/ocrad/) from source bacause standard distribution doesn't work for me...
 cd /tmp
-wget http://ftp.heanet.ie/mirrors/gnu/ocrad/ocrad-0.21.tar.gz -O ocrad.tgz
+wget $OCRAD_LOCATION -O ocrad.tgz
 tar -zxvf ocrad.tgz
-cd ocrad-0.21/
+cd ocrad-${OCRAD_VERSION}/
 ./configure
 make
 sudo make install
 cd ..
-rm -rf ocrad-0.21/
+rm -rf ocrad-${OCRAD_VERSION}/
 rm ocrad.tgz
 
 # installing special patched version of gocr
 cd /tmp
-wget http://sourceforge.net/projects/osra/files/gocr-patched/gocr-0.50pre-patched.tgz
-tar -zxvf gocr-0.50pre-patched.tgz
-cd gocr-0.50pre-patched/
+wget $GOCR_LOCATION -O gocr.tgz
+tar -zxvf gocr.tgz
+cd gocr-${GOCR_VERSION}/
 ./configure
 make libs
 sudo make install
 cd ..
-rm -rf gocr-0.50pre-patched/
-rm gocr-0.50pre-patched.tgz
+rm -rf gocr-${GOCR_VERSION}/
+rm gocr.tgz
 
 # finally, installing osra itself
 cd /tmp
-wget sourceforge.net/projects/osra/files/osra/2.0.0/osra-2.0.0.tgz
-tar -zxvf osra-2.0.0.tgz
-cd osra-2.0.0/
+wget $OSRA_LOCATION -O osra.tgz
+tar -zxvf osra.tgz
+cd osra-${OSRA_VERSION}/
 ./configure
 make all
 sudo make install
 cd ..
-rm -rf osra-2.0.0/
-rm osra-2.0.0.tgz
+rm -rf osra-${OSRA_VERSION}/
+rm osra.tgz

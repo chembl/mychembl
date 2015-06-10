8be80a1f8ddf47b5126b80743d2b6a5ca05a2ca1
Michał Nowotka <mmmnow@gmail.com>
2015-06-10 17:38:36 +0100

    Update ipynb_deamonise.sh

--- ipython_notebooks/ipynb_deamonise.sh
+++ ipython_notebooks/ipynb_deamonise.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-sudo mkdir -p /etc/supervisor.d
+SUPERVISOR_DIR="/etc/supervisor.d"
 
-sudo curl -o /etc/supervisor.d/supervisor.conf https://raw.githubusercontent.com/chembl/mychembl/master/configuration/supervisor.conf
-sudo curl -o /etc/supervisor.d/ipynb.ini https://raw.githubusercontent.com/chembl/mychembl/master/configuration/ipynb.ini
-python -mplatform | grep Ubuntu && sudo curl -o /etc/init.d/supervisord https://raw.githubusercontent.com/chembl/mychembl/master/configuration/supervisord || sudo curl -o /usr/lib/systemd/system/supervisord.service https://raw.githubusercontent.com/chembl/mychembl/master/configuration/supervisord.service
+sudo mkdir -p $SUPERVISOR_DIR
 
-python -mplatform | grep Ubuntu && sudo chmod +x /etc/init.d/supervisord
-python -mplatform | grep Ubuntu && sudo update-rc.d supervisord defaults || sudo systemctl enable supervisord
-python -mplatform | grep Ubuntu && sudo service supervisord start || sudo systemctl start supervisord
+sudo curl -o $SUPERVISOR_DIR/supervisor.conf $RAW/configuration/supervisor.conf
+sudo curl -o $SUPERVISOR_DIR/ipynb.ini $RAW/configuration/ipynb.ini
+
+if [ $(python -mplatform | grep Ubuntu) ]
+   then
+       sudo curl -o /etc/init.d/supervisord $RAW/configuration/supervisord
+       sudo chmod +x /etc/init.d/supervisord
+       sudo update-rc.d supervisord defaults
+       sudo service supervisord start
+   else
+       sudo curl -o /usr/lib/systemd/system/supervisord.service $RAW/configuration/supervisord.service
+       sudo systemctl enable supervisord
+       sudo systemctl start supervisord
+fi

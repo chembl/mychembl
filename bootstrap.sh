ed828d01854f63534381b038932cd286e806796d
Michał Nowotka <mmmnow@gmail.com>
2015-06-10 16:42:14 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# location of raw files of this repository
+export RAW="https://raw.githubusercontent.com/chembl/mychembl/master"
+
 sudo apt-get install -y wget
 
-wget https://raw.githubusercontent.com/chembl/mychembl/master/install_core_libs.sh && bash install_core_libs.sh
-wget https://raw.githubusercontent.com/chembl/mychembl/master/install_py_libs.sh && bash install_py_libs.sh
-wget https://raw.githubusercontent.com/chembl/mychembl/master/ensure_ipv6.sh && bash ensure_ipv6.sh
+wget $RAW/settings.sh && source settings.sh
+wget $RAW/install_core_libs.sh && bash install_core_libs.sh
+wget $RAW/install_py_libs.sh && bash install_py_libs.sh
+wget $RAW/ensure_ipv6.sh && bash ensure_ipv6.sh
 
 sudo useradd -G sudo -s /bin/bash -m chembl
 sudo echo "chembl:chemblvm"| sudo chpasswd
@@ -20,26 +24,26 @@ sudo swapon /swapfile
 echo "vagrant" | sudo -Su postgres createuser -dsr chembl # sudo -u postgres createuser -dsr chembl
 
 cd /tmp
-sudo curl -o /etc/network/interfaces https://raw.githubusercontent.com/chembl/mychembl/master/configuration/mychembl_interfaces
-sudo curl -o /etc/init/failsafe.conf https://raw.githubusercontent.com/chembl/mychembl/master/configuration/failsafe.conf
-sudo curl -o /etc/profile.d/mychembl.sh https://raw.githubusercontent.com/chembl/mychembl/master/configuration/mychembl_bashrc
-sudo curl -o /etc/init/mychembl-upnp.conf https://raw.githubusercontent.com/chembl/mychembl/master/zeroconf/mychembl-upnp.conf
-sudo curl -o /etc/avahi/services/mychembl.service https://raw.githubusercontent.com/chembl/mychembl/master/zeroconf/mychembl.service
-sudo curl -o /usr/bin/mychembl-upnp.py https://raw.githubusercontent.com/chembl/mychembl/master/zeroconf/mychembl-upnp.py
+sudo curl -o /etc/network/interfaces $RAW/configuration/mychembl_interfaces
+sudo curl -o /etc/init/failsafe.conf $RAW/configuration/failsafe.conf
+sudo curl -o /etc/profile.d/mychembl.sh $RAW/configuration/mychembl_bashrc
+sudo curl -o /etc/init/mychembl-upnp.conf $RAW/zeroconf/mychembl-upnp.conf
+sudo curl -o /etc/avahi/services/mychembl.service $RAW/zeroconf/mychembl.service
+sudo curl -o /usr/bin/mychembl-upnp.py $RAW/zeroconf/mychembl-upnp.py
 sudo chmod +x /usr/bin/mychembl-upnp.py
 sudo mkdir /usr/share/themes/mychembl
-sudo curl -o /usr/share/themes/mychembl/mychembl.png https://raw.githubusercontent.com/chembl/mychembl/master/branding/mychembl.png
-sudo curl -o /lib/plymouth/themes/ubuntu-text/ubuntu-text.plymouth https://github.com/chembl/mychembl/blob/master/branding/ubuntu-text.plymouth
+sudo curl -o /usr/share/themes/mychembl/mychembl.png $RAW/branding/mychembl.png
+sudo curl -o /lib/plymouth/themes/ubuntu-text/ubuntu-text.plymouth $RAW/branding/ubuntu-text.plymouth
 
-wget https://raw.githubusercontent.com/chembl/mychembl/master/osra.sh && bash osra.sh
-wget https://raw.githubusercontent.com/chembl/mychembl/master/rdkit_install.sh && sudo su -c "bash rdkit_install.sh" chembl
-wget https://raw.githubusercontent.com/chembl/mychembl/master/ipynb_setup.sh && sudo su -c "bash ipynb_setup.sh" chembl
-wget https://raw.githubusercontent.com/chembl/mychembl/master/create_db.sh && sudo su -c "bash create_db.sh" chembl
-wget https://raw.githubusercontent.com/chembl/mychembl/master/webservices/ws_setup.sh && sudo su -c "bash ws_setup.sh" chembl
-wget https://raw.githubusercontent.com/chembl/mychembl/master/beaker/install_beaker.sh && sudo su -c "bash install_beaker.sh" chembl
-wget https://raw.githubusercontent.com/chembl/mychembl/master/ipython_notebooks/ipynb_deamonise.sh && bash ipynb_deamonise.sh
-wget https://raw.githubusercontent.com/chembl/mychembl/master/apache.sh && bash apache.sh
-wget https://raw.githubusercontent.com/chembl/mychembl/master/launchpad.sh && bash launchpad.sh
+wget $RAW/osra.sh && bash osra.sh
+wget $RAW/rdkit_install.sh && sudo su -c "bash rdkit_install.sh" chembl
+wget $RAW/ipynb_setup.sh && sudo su -c "bash ipynb_setup.sh" chembl
+wget $RAW/create_db.sh && sudo su -c "bash create_db.sh" chembl
+wget $RAW/webservices/ws_setup.sh && sudo su -c "bash ws_setup.sh" chembl
+wget $RAW/beaker/install_beaker.sh && sudo su -c "bash install_beaker.sh" chembl
+wget $RAW/ipython_notebooks/ipynb_deamonise.sh && bash ipynb_deamonise.sh
+wget $RAW/apache.sh && bash apache.sh
+wget $RAW/launchpad.sh && bash launchpad.sh
 
 sudo apt-get remove -y cloud-init
 sudo rm /etc/init/cloud-* -rf

c369aa8140696573f2283ba4d4c769e950b6cb99
Michał Nowotka <mmmnow@gmail.com>
2015-06-10 21:09:18 +0100

    Update launchpad.sh

--- launchpad.sh
+++ launchpad.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-python -mplatform | grep Ubuntu && export APACHE_NAME="apache2" || export APACHE_NAME="httpd"
+python -mplatform | grep Ubuntu && APACHE_NAME="apache2" || APACHE_NAME="httpd"
+WWW="/var/www/html/"
 
 cd /tmp
 sudo git clone $MYCHEMBL_WEBAPP
 sudo git clone $MYCHEMBL_GIT
-sudo rm /var/www/html/* -rf
-sudo mkdir -p /var/www/html/mychembl
-sudo cp -r mychembl/launchpad/* /var/www/html/
-sudo cp -r mychembl/launchpad/.htaccess /var/www/html/
-sudo cp -r mychembl_webapp/* /var/www/html/mychembl/
+sudo rm $WWW* -rf
+sudo mkdir -p ${WWW}mychembl
+sudo cp -r mychembl/launchpad/* $WWW
+sudo cp -r mychembl/launchpad/.htaccess $WWW
+sudo cp -r mychembl_webapp/* ${WWW}mychembl/
 wget $JSME_LOCATION -O JSME.zip
 unzip JSME.zip
-sudo mv JSME/jsme /var/www/html/mychembl/static/js/
+sudo mv JSME/jsme ${WWW}mychembl/static/js/
 
 sudo -E kill -9 $(pidof $APACHE_NAME)
 python -mplatform | grep Ubuntu && sudo service apache2 restart || sudo systemctl restart httpd -l

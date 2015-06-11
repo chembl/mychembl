7656f94663d9b31953d2cddddd6c93bffe7183ac
Michał Nowotka <mmmnow@gmail.com>
2015-06-11 17:09:13 +0100

    Update footer.php

--- launchpad/footer.php
+++ launchpad/footer.php
@@ -1,7 +1,7 @@
       <div class="row">
         <div class="footer">
           <div class="col-lg-6">
-            <p><span>myChEMBL 2015</span>|<span>ChEMBL Release XXX</span></p>
+            <p><span>myChEMBL <?php echo date("Y") ?></span>|<span>ChEMBL Release XXX</span></p>
           </div>
           <div class="col-lg-6">
             <p class="text-right"><span><a href="https://www.ebi.ac.uk/chembl/" target="_blank">ChEMBL Website</a></span>|<span><a href="mailto:mychembl@ebi.ac.uk">myChEMBL Support</a></span></p>         

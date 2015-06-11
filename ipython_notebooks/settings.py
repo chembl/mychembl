64997771b63797a50f2c264e32456140917fb238
Michał Nowotka <mmmnow@gmail.com>
2015-06-11 16:43:03 +0100

    Update settings.py

--- ipython_notebooks/settings.py
+++ ipython_notebooks/settings.py
@@ -3,7 +3,7 @@ DEBUG = False
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'chembl_19', # Or path to database file if using sqlite3.
+        'NAME': 'chembl_XXX', # Or path to database file if using sqlite3.
         'USER': 'chembl', # Not used with sqlite3.
         'PASSWORD': '', # Not used witis oracle
         'HOST': '', # Set to empty string for localhost. Not used with sqlite3.

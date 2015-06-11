afa68710fdd74146d346be136c43bf591b29e241
Michał Nowotka <mmmnow@gmail.com>
2015-06-11 10:05:57 +0100

    Update launchpad.sh

--- launchpad.sh
+++ launchpad.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-python -mplatform | grep Ubuntu && APACHE_NAME="apache2" || APACHE_NAME="httpd"
-WWW="/var/www/html/"
+python -mplatform | grep Ubuntu && export APACHE_NAME="apache2" || export APACHE_NAME="httpd"
+export AUX_WWW="/var/www/html/"
 
 cd /tmp
 sudo git clone $MYCHEMBL_WEBAPP
 sudo git clone $MYCHEMBL_GIT
-sudo rm $WWW* -rf
-sudo mkdir -p ${WWW}mychembl
-sudo cp -r mychembl/launchpad/* $WWW
-sudo cp -r mychembl/launchpad/.htaccess $WWW
-sudo cp -r mychembl_webapp/* ${WWW}mychembl/
+sudo -E rm $AUX_WWW* -rf
+sudo -E mkdir -p ${AUX_WWW}mychembl
+sudo -E cp -r mychembl/launchpad/* $AUX_WWW
+sudo -E cp -r mychembl/launchpad/.htaccess $AUX_WWW
+sudo -E cp -r mychembl_webapp/* ${AUX_WWW}mychembl/
 wget $JSME_LOCATION -O JSME.zip
 unzip JSME.zip
-sudo mv JSME/jsme ${WWW}mychembl/static/js/
+sudo -E mv JSME/jsme ${AUX_WWW}mychembl/static/js/
 
 sudo -E kill -9 $(pidof $APACHE_NAME)
 python -mplatform | grep Ubuntu && sudo service apache2 restart || sudo systemctl restart httpd -l

df68fac9572643b9b4f11dccb01240499ee9f0a3
Michał Nowotka <mmmnow@gmail.com>
2015-06-18 12:47:28 +0100

    Update launchpad.sh

--- launchpad.sh
+++ launchpad.sh
@@ -11,6 +11,10 @@ sudo -E mkdir -p ${AUX_WWW}mychembl
 sudo -E cp -r mychembl/launchpad/* $AUX_WWW
 sudo -E cp -r mychembl/launchpad/.htaccess $AUX_WWW
 sudo -E cp -r mychembl_webapp/* ${AUX_WWW}mychembl/
+sudo curl -o ${AUX_WWW}mychembl/config/config.php $RAW/configuration/webapp_config.php
+sudo -E sed -i "s/chembl_XXX/chembl_${CHEMBL_VERSION}/g" ${AUX_WWW}mychembl/config/config.php
+sudo -E sed -i "s/ChEMBL_XXX/ChEMBL_${CHEMBL_VERSION}/g" ${AUX_WWW}mychembl/config/config.php
+sudo -E sed -i "s/Release XXX/Release ${CHEMBL_VERSION}/g" ${AUX_WWW}*.php
 sudo -E sed -i "s/Release XXX/Release ${CHEMBL_VERSION}/g" ${AUX_WWW}*.php
 sudo -E sed -i "s/myChEMBLXXX_KNIME.zip/myChEMBL${CHEMBL_VERSION}_KNIME.zip/g" ${AUX_WWW}knime.php
 

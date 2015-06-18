85c4c7e7cfeb733f0543e9435a16972cd61350bb
Michał Nowotka <mmmnow@gmail.com>
2015-06-18 12:40:32 +0100

    Create webapp_config.php

---
+++ configuration/webapp_config.php
@@ -0,0 +1,29 @@
+<?php
+
+ /*
+===============
+
+(c) 2012 EMBL European Molecular Biology Laboratories
+
+This code is licensed under Version 2.0 of the Open Source Initiative Apache License.
+URL: http://www.opensource.org/licenses/apache2.0.php 
+
+===============
+ */
+
+  // Application Properties
+ 
+  # Application Settings
+  $app_title      = "myChEMBL";
+  $basedir        = "/var/www/html/mychembl/";
+  $app2base       = "../";
+  $tmpdir         = "/tmp";
+  $chembl_version = "ChEMBL_XXX";
+  $ws_base_url    = "";
+  
+  # Database Settings
+  $db_user = "chembl"; 
+  $db_name = "chembl_XXX";
+  $db_host = "/var/run/postgresql";
+  $db_port = 5432;  
+?>

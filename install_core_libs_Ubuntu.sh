c0c63fda3a89fc7a8188acb6cbc4131de2c4a4bc
Michał Nowotka <mmmnow@gmail.com>
2015-07-13 21:50:01 +0100

    Update install_core_libs_Ubuntu.sh

--- install_core_libs_Ubuntu.sh
+++ install_core_libs_Ubuntu.sh
@@ -52,5 +52,6 @@ sudo apt-get install -y phppgadmin
 sudo apt-get install -y libapache2-mod-wsgi
 sudo apt-get install -y ruby
 sudo apt-get install -y iptables
+sudo apt-get install -y curl
 
 sudo gem install gist

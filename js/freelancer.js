35b60be8d0b34cc2899fd9a243dd9ee502eae867
Michal Nowotka <mnowotka@ebi.ac.uk>
2016-01-13 15:22:05 +0000

    more stuff

--- js/freelancer.js
+++ js/freelancer.js
@@ -29,9 +29,42 @@ $(function() {
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
     target: '.navbar-fixed-top'
-})
+});
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
+});
+
+// Remember chosen OS
+$(function() {
+    var chosen_os = null;
+ $('.oses .portfolio-link', '#install').click(function(){
+     $('.oses .portfolio-link', '#install').removeClass('selected');
+     var classes = $(this).attr("class").split(' ');
+     for(var i = 0; i < classes.length; i++){
+         if (classes[i]!="portfolio-link"){
+             chosen_os = classes[i].toLowerCase();
+             break;
+         }
+     }
+     if (chosen_os){
+         $('span.command').text(function(){
+                 var ret = $(this).text();
+                 ret = ret.replace('ubuntu', chosen_os);
+                 ret = ret.replace('centos', chosen_os);
+                 return ret;
+             }
+
+         );
+
+         $('a.download-link').each(function(){
+            if(chosen_os == 'centos')
+                this.href = 'http://ftp.ebi.ac.uk/pub/databases/chembl/VM/myChEMBL/releases/myChEMBL-20_0/CentOS/VirtualBox/centos.vmdk.tar.gz';
+            else
+                this.href = 'http://ftp.ebi.ac.uk/pub/databases/chembl/VM/myChEMBL/releases/myChEMBL-20_0/Ubuntu/VirtualBox/ubuntu.vmdk.tar.gz';
+         });
+     }
+     $(this).addClass('selected');
+ });
 });

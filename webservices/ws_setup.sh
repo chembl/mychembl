c53313b28380b179075dc18fcfa38a2a76de50f8
Michał Nowotka <mmmnow@gmail.com>
2016-04-21 18:28:02 +0100

    Update ws_setup.sh

--- webservices/ws_setup.sh
+++ webservices/ws_setup.sh
@@ -6,12 +6,13 @@
 # Created date: 20.05.2014
 
 export HOME=/home/chembl
-export VIRTUALENVWRAPPER_PYTHON=$(which python)
-export PATH=/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/home/chembl/.local/bin:/home/chembl/bin
-export PYTHONPATH=$HOME/rdkit:$HOME/indigo
-export RDBASE=$HOME/rdkit
-export LD_LIBRARY_PATH=$RDBASE/lib:$LD_LIBRARY_PATH
-export WORKON_HOME=$HOME/.virtualenvs
+#export VIRTUALENVWRAPPER_PYTHON=$(which python)
+#export PATH=/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/home/chembl/.local/bin:/home/chembl/bin
+#export PYTHONPATH=$HOME/rdkit:$HOME/indigo
+export PYTHONPATH=$HOME/indigo
+#export RDBASE=$HOME/rdkit
+#export LD_LIBRARY_PATH=$RDBASE/lib:$LD_LIBRARY_PATH
+#export WORKON_HOME=$HOME/.virtualenvs
 
 cd $HOME
 
@@ -22,32 +23,32 @@ mv $INDIGO_FILENAME indigo
 rm indigo.zip
 
 # Source virtualenvwrapper because it makes our job lot easier:
-source virtualenvwrapper.sh
+#source virtualenvwrapper.sh
 
 # Use the wrapper to create new virtualenv called chembl_webservices
-mkvirtualenv chembl_webservices
+#mkvirtualenv chembl_webservices
 
 # OK, we are ready to use pip to install chembl_webservices
-pip install chembl_webservices
+#pip install chembl_webservices
 
 # We need live online documentation as well
-pip install tastypie_spore_docs
+#pip install tastypie_spore_docs
 
 # Because webservices on myChEMBL VM will be run against exported CHEMBL schema,
 # which is only a subset of chembl_app schema we will have to install
 # a compatibility layer that can handle differences between schemas:
-pip install 'chembl_compatibility>=2.0.0'
+#pip install 'chembl_compatibility>=2.0.0'
 
 # webservices are DB agnostic so they are not shipped with any specific DB
 # driver. We have to install the driver separately, in case of myChEMBL this
 # will be postgres:
-echo $(env)
-echo $(which pg_config)
-pip install psycopg2
+#echo $(env)
+#echo $(which pg_config)
+#pip install psycopg2
 
 # we want our application to support CORS so we have to install django app
 # which will handle this:
-pip install django-cors-headers
+#pip install django-cors-headers
 
 # OK, now we are ready to configure our webservices django app.
 # First, lets create new django project:

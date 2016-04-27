675be4c91d444094b895bd9d30e99ac8433110d4
Michał Nowotka <mmmnow@gmail.com>
2016-04-27 13:49:17 +0100

    Update beaker.py

--- beaker/beaker.py
+++ beaker/beaker.py
@@ -3,8 +3,6 @@ import sys
 import site
 site.addsitedir('/home/chembl/.virtualenvs/chembl_beaker/lib/python2.7/site-packages/')
 sys.path.append('/home/chembl/chembl_beaker/conf.d')
-sys.path.append('/home/chembl/rdkit')
-os.environ['LD_LIBRARY_PATH']='/home/chembl/rdkit/lib'
 
 from bottle import debug
 import json
